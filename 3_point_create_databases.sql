fd4d59368e4316cca880d8273d07d5c4b19a3e9c
Anton Pozdniakov <82996880+AntonStart@users.noreply.github.com>
2024-06-27 15:21:45 +0400

    Create 3_point_create_databases.sql

---
+++ 3_point_create_databases.sql
@@ -0,0 +1,8 @@
+-- База с буферными таблицами
+CREATE DATABASE direct_log;
+-- Слой сырых данных
+CREATE DATABASE stg;
+-- Исторических данных
+CREATE DATABASE history;
+-- Слой текущих данных
+CREATE DATABASE currently;
